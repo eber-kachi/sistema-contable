db50a686d2800a0f96e1f2dbda56b7043f30284c
anghelox14 <62817099+anghelox14@users.noreply.github.com>
2020-04-13 00:27:31 -0400

    backup library implemented

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('backup:clean')->daily()->at('00:02');
+        $schedule->command('backup:run')->daily()->at('00:18');
     }
 
     /**
@@ -39,4 +39,5 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
 }
